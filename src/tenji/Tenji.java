9c6cb442c3002203d4adcf0ed87537e10f073ab1
TANAKA Hidemune <auau@kne.biglobe.ne.jp>
2014-04-11 19:59:17 +0900

    Signed-off-by: TANAKA Hidemune <auau@kne.biglobe.ne.jp>

---
+++ src/tenji/Tenji.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tenji;
+
+/**
+ *
+ * @author HDM
+ */
+public class Tenji {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TenjiJFrame frm = new TenjiJFrame();
+        frm.setVisible(true);
+    }
+}
